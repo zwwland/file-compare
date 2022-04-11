0a88ae85122ecabb3efc9f041df4598268114c70
benchu1k <benchu1k@gmail.com>
2022-04-11 19:20:14 +0800

    1

--- src/api.php
+++ src/api.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once '../vendor/autoload.php';
+
 use TencentCloud\Common\Credential;
 use TencentCloud\Common\Profile\ClientProfile;
 use TencentCloud\Common\Profile\HttpProfile;
@@ -9,6 +10,8 @@ use TencentCloud\Ocr\V20181119\OcrClient;
 use TencentCloud\Ocr\V20181119\Models\GeneralAccurateOCRRequest;
 use TencentCloud\Ocr\V20181119\Models\GeneralHandwritingOCRRequest;
 
+error_reporting(E_ALL);
+
 $dotenv = \Dotenv\Dotenv::createMutable(__DIR__);
 $dotenv->load();
 $id  = $_ENV['APP_ID'];
@@ -16,10 +19,12 @@ $sec = $_ENV['APP_SECRET'];
 $s   = $_POST['source'];
 $n   = $_POST['new'];
 header("Content-Type: application/json; charset=UTF-8");
-if(empty($s) || empty($n)){
+if (empty($s) || empty($n)) {
     echo '{}';
     exit;
 }
+$sData = explode(',', $s);
+$nData = explode(',', $n);
 try {
 
 
@@ -33,13 +38,13 @@ try {
 
     $req = new GeneralAccurateOCRRequest();
     // $req = new GeneralHandwritingOCRRequest();
-    
+
     $params = array(
-        "ImageBase64" => explode(",", $s)[1],
+        "ImageBase64" => $sData[1],
         //手写体
         // "EnableWordPolygon "=> true,
-        "EnableDetectSplit" => true,
-        "IsWords"=> true
+        "EnableDetectSplit" => false,
+        "IsWords" => true
     );
     $req->fromJsonString(json_encode($params));
 
@@ -50,9 +55,9 @@ try {
     $data['source'] = json_decode($resp->toJsonString(), true);
 
     $params = array(
-        "ImageBase64" => explode(",", $n)[1],
-        "EnableDetectSplit" => true,
-        "IsWords"=> true
+        "ImageBase64" => $nData[1],
+        "EnableDetectSplit" => false,
+        "IsWords" => true
     );
     $req->fromJsonString(json_encode($params));
 
@@ -60,116 +65,241 @@ try {
     $resp = $client->GeneralAccurateOCR($req);
     $data['new'] = json_decode($resp->toJsonString(), true);
 
-    $a = '';
-    foreach($data['source']['TextDetections'] as $k => $v){
-        $a = $a . $v['DetectedText'] ;
+    $reg = ["\t", "\r", "\n", '\\', '/', '「', '」', '-', '=', '，', '。', '.', '；', ';', '、', '：', ':', '(', ')', '（', '）', '[', ']', '【', '】', '"', "'", '‘', '’', '“', '”', '>', '<', '》', '《', ' ', '_', '凵','↵'];
+
+    //原字符串处理
+    $sourceData = [];
+    foreach ($data['source']['TextDetections'] as $s1) {
+        foreach ($s1['Words'] as $k => $w) {
+            if (in_array($w['Character'], $reg)) {
+                continue;
+            }
+            $tmp = [
+                'word' => $w,
+                'coord' => $s1['WordCoordPoint'][$k]
+            ];
+            $sourceData[] = $tmp;
+        }
     }
-    $b = '';
-    foreach($data['new']['TextDetections'] as $k => $v){
-        $b = $b . $v['DetectedText'] ;
+
+    //新字符串处理
+    $newData = [];
+    foreach ($data['new']['TextDetections'] as $n1) {
+        foreach ($n1['Words'] as $k => $w) {
+            if (in_array($w['Character'], $reg)) {
+                continue;
+            }
+            $tmp = [
+                'word' => $w,
+                'coord' => $n1['WordCoordPoint'][$k]
+            ];
+            $newData[] = $tmp;
+        }
     }
+
+    $a = array_reduce($sourceData, fn ($a, $b) => $a . $b['word']['Character'], '');
+    $b = array_reduce($newData, fn ($a, $b) => $a . $b['word']['Character'], '');
+
     $res = http_curl_json('http://stamper.998990.xyz:8086', [
         'a' => $a,
         'b' => $b
-    ],'POST');
+    ], 'POST');
     $res_decode = json_decode($res, true);
-    // file_put_contents('./tmp.png', base64_decode(explode(',',$n)[1]));
-    // $s = draw('./tmp.png', $data);
 
-    echo json_encode($data);
-}catch(TencentCloudSDKException $e) {
+    $polygons = [];
+    $len = mb_strlen($res_decode['alignedSequences'][0]);
+
+
+    $x = 0;
+    $y = 0;
+    $xDiff = [];
+    $yDiff = [];
+    for ($i = 0; $i < $len; $i++) {
+        $tx = mb_substr($res_decode['alignedSequences'][0], $i, 1);
+        $ty = mb_substr($res_decode['alignedSequences'][1], $i, 1);
+
+        if($tx === '⭐' && $ty === '⭐')
+        {
+            // $xDiff[] = [
+            //     'color' => 'green',
+            //     'coord' => $sourceData[$x]['coord']
+            // ];
+            // $yDiff[] = [
+            //     'color' => 'green',
+            //     'coord' => $newData[$y]['coord']
+            // ];
+            // $x++;
+            // $y++;
+            continue;
+        }
+
+        else if($tx === '⭐' && $ty !== '⭐')
+        {
+            $yDiff[] = [
+                'color' => 'green',
+                'coord' => $newData[$y]['coord']
+            ];
+            $y++;
+        }
+
+        else if($tx !== '⭐' && $ty === '⭐')
+        {
+            $xDiff[] = [
+                'color' => 'green',
+                'coord' => $sourceData[$x]['coord']
+            ];
+            $x++;
+        }
+        else {
+            $x++;
+            $y++;
+        }
+
+    }
+
+    $r = time() . random_int(100000, 999999);
+    if (str_contains($sData[0], 'jpeg')) {
+        $sExt = 'jpeg';
+    }
+    if (str_contains($sData[0], 'png')) {
+        $sExt = 'png';
+    }
+    $oldImg = 'img/'.$r.'tmp-old.' . $sExt;
+    $oldImgNew = 'img/'.$r.'tmp-old-1.' . $sExt;
+
+    if (str_contains($nData[0], 'jpeg')) {
+        $nExt = 'jpeg';
+    }
+    if (str_contains($nData[0], 'png')) {
+        $nExt = 'png';
+    }
+    $newImg = 'img/'.$r.'tmp-new.'  . $nExt;
+    $newImgNew = 'img/'.$r.'tmp-new-1.' . $nExt;
+
+
+    file_put_contents($oldImg, base64_decode($sData[1]));
+    draw($oldImg, $xDiff, $oldImgNew);
+    file_put_contents($newImg, base64_decode($nData[1]));
+    draw($newImg, $yDiff, $newImgNew);
+
+    echo json_encode([
+        's' => './' . $oldImgNew . '?v=' . time() . random_int(0, 10000),
+        'n' => './' . $newImgNew . '?v=' . time() . random_int(0, 10000),
+    ]);
+
+    // echo json_encode(['s'=>base64_encode(file_get_contents('tmp-old-1.png')), 'n'=>base64_encode(file_get_contents('tmp-new-1.png'))]);
+} catch (TencentCloudSDKException $e) {
     echo $e;
 }
 
 
-function draw($imagePos, $data)
+function draw($imagePos, $data, $outputPos)
 {
-    
-    return '';
-    
+    $info = pathinfo($imagePos);
+
+    switch ($info['extension']) {
+        case 'jpeg':
+        case 'jpg':
+            $img = imagecreatefromjpeg($imagePos);
+            break;
+        case 'png':
+            $img = imagecreatefrompng($imagePos);
+            break;
+        case 'gif':
+            $img = imagecreatefromgif($imagePos);
+            break;
+        default:
+            die('不支持的图片文件类型');
+            break;
+    }
+
+    $green = imagecolorallocate($img, 0, 255, 0);
+
+    foreach ($data as $d) {
+        $coord = array_map(function ($n) {
+            return [$n['X'], $n['Y']];
+        }, $d['coord']['WordCoordinate']);
+
+        $coord2 = [];
+        foreach ($coord as $c) {
+            $coord2 = array_merge($coord2, array_values($c));
+        }
+        imagepolygon($img, $coord2, 4, $green);
+    }
+
+    switch ($info['extension']) {
+        case 'jpeg':
+        case 'jpg':
+            imagejpeg($img, $outputPos);
+            break;
+        case 'png':
+            imagepng($img, $outputPos);
+            break;
+        case 'gif':
+            imagegif($img, $outputPos);
+            break;
+        default:
+            die('不支持的图片文件类型');
+            break;
+    }
+
+    imagedestroy($img);
+
+    return true;
 }
 
-function send_post($url, $post_data) {
-  
-
-    $options = array(
-  
-      'http' => array(
-  
-        'method' => 'POST',
-  
-        'header' => 'Content-type:application/json',
-  
-        'content' => json_encode($post_data),
-  
-        'timeout' => 15 * 60 // 超时时间（单位:s）
-  
-      )
-  
-    );
-  
-    $context = stream_context_create($options);
-  
-    $result = file_get_contents($url, false, $context);
-  
-     
-    return $result;
-  
-  }
-
-  function http_curl_json($uri, $post_data = [],  $method = 'GET', $header = [])
-  {
-      //初始化
-      $ch      = curl_init();
+function http_curl_json($uri, $post_data = [],  $method = 'GET', $header = [])
+{
+    //初始化
+    $ch      = curl_init();
     //   $reqdata = http_build_query($post_data);
     //   $length  = strlen($reqdata);
     $reqdata = json_encode($post_data);
     $length = strlen($reqdata);
-      curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-      //异步 1 非异步 0
-      curl_setopt($ch, CURLOPT_TIMEOUT, 0);
-      if (preg_match('/:(\d+)/', $uri, $port)) {
-          $url = str_replace(':' . $port[1], '', $uri);
-          $port = $port[1];
-      } else {
-          $url  = $uri;
-          $port = 80;
-      }
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_PORT, $port);
-      curl_setopt($ch, CURLOPT_HEADER, 0);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      if (strtoupper($method) == 'POST') {
-          curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    //异步 1 非异步 0
+    curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+    if (preg_match('/:(\d+)/', $uri, $port)) {
+        $url = str_replace(':' . $port[1], '', $uri);
+        $port = $port[1];
+    } else {
+        $url  = $uri;
+        $port = 80;
+    }
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_PORT, $port);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if (strtoupper($method) == 'POST') {
+        curl_setopt($ch, CURLOPT_POST, 1);
         //   curl_setopt($ch, CURLOPT_POSTFIELDS, $reqdata);
-          curl_setopt($ch, CURLOPT_POSTFIELDS, $reqdata);
-          array_push($header, 'Content-length: ' . $length);
-      }
-      //    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
-      //    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
-      $default_header = [
-          // 'Content-Type: application/x-www-form-urlencoded;charset=utf-8',
-          'Content-Type: application/json; charset=utf-8',
-          //"Content-Length: {$length}",
-          //'X-Requested-With: XMLHttpRequest',
-          //'Accept: application/json, text/javascript, */*; q=0.01',
-          //'Accept-Encoding: gzip, deflate',
-          //'Accept-Language: en-US,en;q=0.9',
-          //'Pragma: no-cache',
-          'Cache-Control: no-cache',
-          //                    'User-Agent: PostmanRuntime/7.15.0',
-          'Accept: */*',
-          //                    'accept-encoding: gzip,deflate',
-      ];
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array_merge($default_header, $header));
-  
-      $data = curl_exec($ch);
-      // $s = curl_getinfo($ch,CURLINFO_HEADER_OUT);
-      if (false === $data) {
-          $err = curl_error($ch);
-      }
-      curl_close($ch);
-  
-      return $data;
-  }
-  
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $reqdata);
+        array_push($header, 'Content-length: ' . $length);
+    }
+    //    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
+    //    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
+    $default_header = [
+        // 'Content-Type: application/x-www-form-urlencoded;charset=utf-8',
+        'Content-Type: application/json; charset=utf-8',
+        //"Content-Length: {$length}",
+        //'X-Requested-With: XMLHttpRequest',
+        //'Accept: application/json, text/javascript, */*; q=0.01',
+        //'Accept-Encoding: gzip, deflate',
+        //'Accept-Language: en-US,en;q=0.9',
+        //'Pragma: no-cache',
+        'Cache-Control: no-cache',
+        //                    'User-Agent: PostmanRuntime/7.15.0',
+        'Accept: */*',
+        //                    'accept-encoding: gzip,deflate',
+    ];
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array_merge($default_header, $header));
+
+    $data = curl_exec($ch);
+    // $s = curl_getinfo($ch,CURLINFO_HEADER_OUT);
+    if (false === $data) {
+        $err = curl_error($ch);
+    }
+    curl_close($ch);
+
+    return $data;
+}
--- src/diff.js
+++ src/diff.js
@@ -27,93 +27,96 @@
                 fetch("/api.php", { method: "POST", body: formData })
                   .then((res) => res.json())
                   .then((res) => {
-                    let source = []
-                    if(res.source.TextDetections.length > 0)
-                    {
-                      const basicSourceDiff = (res.source.TextDetections[0].Polygon[0].Y - res.source.TextDetections[0].Polygon[0].Y) ** 2
-                      let basicSourceLastY = res.source.TextDetections[0].Polygon[0].Y
+                    document.querySelector(".item-source").src = res.s
+                    document.querySelector('.item-new').src = res.n
 
-                      for(const text of res.source.TextDetections)
-                      {
-                        let diff = (text.Polygon[0].Y - basicSourceLastY - basicSourceDiff) ** 2
-                        if (diff < 26)
-                        {
-                          if(source.length == 0)
-                          {
-                            source.push(text)
-                          }else{
-                            source[source.length - 1 ].DetectedText += text.DetectedText
-                            source[source.length - 1 ].ItemPolygon.Width += text.ItemPolygon.Width
-                            // source[source.length - 1 ].ItemPolygon.Height += text.ItemPolygon.Height
-                            source[source.length - 1 ].WordCoordPoint.push(...text.WordCoordPoint)
-                            source[source.length - 1 ].Words.push(...text.Words)
-                          }
-                        }else{
-                          source.push(text)
-                        }
-                          basicSourceLastY = text.Polygon[0].Y
-                      }
-                    }
-                    let ne = []
-                    if(res.new.TextDetections.length > 0)
-                    {
-                      const basicNewDiff = (res.new.TextDetections[0].Polygon[0].Y - res.new.TextDetections[0].Polygon[0].Y) ** 2
-                      let basicNewLastY = res.new.TextDetections[0].Polygon[0].Y
+                    // let source = []
+                    // if(res.source.TextDetections.length > 0)
+                    // {
+                    //   const basicSourceDiff = (res.source.TextDetections[0].Polygon[0].Y - res.source.TextDetections[0].Polygon[0].Y) ** 2
+                    //   let basicSourceLastY = res.source.TextDetections[0].Polygon[0].Y
 
-                      for(const text of res.new.TextDetections)
-                      {
-                        let diff = (text.Polygon[0].Y - basicNewLastY - basicNewDiff) ** 2
-                        if (diff < 26) {
-                          if(ne.length == 0)
-                          {
-                            ne.push(text)
-                          }else{
-                            ne[ne.length - 1 ].DetectedText += text.DetectedText
-                            ne[ne.length - 1 ].ItemPolygon.Width += text.ItemPolygon.Width
-                            // ne[ne.length - 1 ].ItemPolygon.Height += text.ItemPolygon.Height
-                            ne[ne.length - 1 ].WordCoordPoint.push(...text.WordCoordPoint)
-                            ne[ne.length - 1 ].Words.push(...text.Words)
-                          }
-                        }else{
-                          ne.push(text)
-                        }
-                        basicNewLastY = text.Polygon[0].Y
-                      }
-                    }
-                    const s = source.map(
-                      (item) => item.DetectedText.replace(/[\s\,「」\-\="'，。\.；;、\\/ ：:()（）\[\]【】 “” > < 》 《]/g, "")
-                    ).join("✌");
-                    const n = ne.map(
-                      (item) => item.DetectedText.replace(/[\s\,「」\-\="'，。\.；;、\\/ ：:()（）\[\]【】 “” > < 》 《]/g, "")
-                    ).join("✌");
-                    console.log(s,n,source,ne)
-                    let al = aligner.NWaligner({
-                      gapSymbol: "⭐"
-                    })
-                    // let start = (new Date()).getTime();
-                    let r = al.align(s,n)
-                    // let start1 = (new Date()).getTime();
-                    let ssequence = r.alignedSequences[0].split("")
-                    let nsequence = r.alignedSequences[1].split("")
-                    for(let i=0; i < ssequence.length; i++) {
-                      if(ssequence[i] == "⭐" && nsequence[i] == "⭐") {
-                        nsequence[i] = ""
-                      }
-                      if(ssequence[i] != '⭐' && nsequence[i] == '⭐') {
-                        nsequence[i] = `<span style="border:1px solid red;">${ssequence[i]}</span>`
-                      }
-                      if(ssequence[i] == '⭐' && nsequence[i] != '⭐') {
-                        nsequence[i] = `<span style="border:1px solid green">${nsequence[i]}</span>`
-                      }
-                    }
-                    // let start2 = (new Date()).getTime();
-                    document.querySelector(".item-new").innerHTML =  nsequence.join('').replace(/✌/g, "<br />");
-                    document.querySelector(".item-source").innerHTML = s.replace(/✌/g, "<br>");
-                    // let start3 = (new Date()).getTime();
-                    // console.log(start3-start2, start2-start1, start1 - start);
-                    var sourceFragment = document.createDocumentFragment();
-                    sourceFragment.appendChild(document.createTextNode(s));
-                  });
+                    //   for(const text of res.source.TextDetections)
+                    //   {
+                    //     let diff = (text.Polygon[0].Y - basicSourceLastY - basicSourceDiff) ** 2
+                    //     if (diff < 26)
+                    //     {
+                    //       if(source.length == 0)
+                    //       {
+                    //         source.push(text)
+                    //       }else{
+                    //         source[source.length - 1 ].DetectedText += text.DetectedText
+                    //         source[source.length - 1 ].ItemPolygon.Width += text.ItemPolygon.Width
+                    //         // source[source.length - 1 ].ItemPolygon.Height += text.ItemPolygon.Height
+                    //         source[source.length - 1 ].WordCoordPoint.push(...text.WordCoordPoint)
+                    //         source[source.length - 1 ].Words.push(...text.Words)
+                    //       }
+                    //     }else{
+                    //       source.push(text)
+                    //     }
+                    //       basicSourceLastY = text.Polygon[0].Y
+                    //   }
+                    // }
+                    // let ne = []
+                    // if(res.new.TextDetections.length > 0)
+                    // {
+                    //   const basicNewDiff = (res.new.TextDetections[0].Polygon[0].Y - res.new.TextDetections[0].Polygon[0].Y) ** 2
+                    //   let basicNewLastY = res.new.TextDetections[0].Polygon[0].Y
+
+                    //   for(const text of res.new.TextDetections)
+                    //   {
+                    //     let diff = (text.Polygon[0].Y - basicNewLastY - basicNewDiff) ** 2
+                    //     if (diff < 26) {
+                    //       if(ne.length == 0)
+                    //       {
+                    //         ne.push(text)
+                    //       }else{
+                    //         ne[ne.length - 1 ].DetectedText += text.DetectedText
+                    //         ne[ne.length - 1 ].ItemPolygon.Width += text.ItemPolygon.Width
+                    //         // ne[ne.length - 1 ].ItemPolygon.Height += text.ItemPolygon.Height
+                    //         ne[ne.length - 1 ].WordCoordPoint.push(...text.WordCoordPoint)
+                    //         ne[ne.length - 1 ].Words.push(...text.Words)
+                    //       }
+                    //     }else{
+                    //       ne.push(text)
+                    //     }
+                    //     basicNewLastY = text.Polygon[0].Y
+                    //   }
+                    // }
+                    // const s = source.map(
+                    //   (item) => item.DetectedText.replace(/[\s\,「」\-\="'，。\.；;、\\/ ：:()（）\[\]【】 “” > < 》 《]/g, "")
+                    // ).join("✌");
+                    // const n = ne.map(
+                    //   (item) => item.DetectedText.replace(/[\s\,「」\-\="'，。\.；;、\\/ ：:()（）\[\]【】 “” > < 》 《]/g, "")
+                    // ).join("✌");
+                    // console.log(s,n,source,ne)
+                    // let al = aligner.NWaligner({
+                    //   gapSymbol: "⭐"
+                    // })
+                    // // let start = (new Date()).getTime();
+                    // let r = al.align(s,n)
+                    // // let start1 = (new Date()).getTime();
+                    // let ssequence = r.alignedSequences[0].split("")
+                    // let nsequence = r.alignedSequences[1].split("")
+                    // for(let i=0; i < ssequence.length; i++) {
+                    //   if(ssequence[i] == "⭐" && nsequence[i] == "⭐") {
+                    //     nsequence[i] = ""
+                    //   }
+                    //   if(ssequence[i] != '⭐' && nsequence[i] == '⭐') {
+                    //     nsequence[i] = `<span style="border:1px solid red;">${ssequence[i]}</span>`
+                    //   }
+                    //   if(ssequence[i] == '⭐' && nsequence[i] != '⭐') {
+                    //     nsequence[i] = `<span style="border:1px solid green">${nsequence[i]}</span>`
+                    //   }
+                    // }
+                    // // let start2 = (new Date()).getTime();
+                    // document.querySelector(".item-new").innerHTML =  nsequence.join('').replace(/✌/g, "<br />");
+                    // document.querySelector(".item-source").innerHTML = s.replace(/✌/g, "<br>");
+                    // // let start3 = (new Date()).getTime();
+                    // // console.log(start3-start2, start2-start1, start1 - start);
+                    // var sourceFragment = document.createDocumentFragment();
+                    // sourceFragment.appendChild(document.createTextNode(s));
+                  }).catch(err=> console.log(err));
               }
               reader.removeEventListener("loadend", this);
             });
